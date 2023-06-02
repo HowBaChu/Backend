f64dba0a2cac5309579c7eb2010c5d802411a359
dlrjs2360 <lhg3670@naver.com>
2023-06-02 10:19:18 +0900

    :tada: setting: 스프링부트 기본 세팅
    - JDK 버전 : 11

---
+++ src/main/java/com/example/howbachu/HowBaChuApplication.java
@@ -0,0 +1,13 @@
+package com.example.howbachu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowBaChuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowBaChuApplication.class, args);
+	}
+
+}

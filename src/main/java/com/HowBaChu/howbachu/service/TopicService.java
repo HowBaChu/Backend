d7baab9f15c68c93b72aeda4257f0f5ab4b2d208
Thingk0 <audtjd9080@naver.com>
2023-08-09 12:25:36 +0900

    :sparkles: 토픽 서비스 동적 쿼리 기능 구현 및 테스트 코드 작성
    - querydsl을 이용하여 동적 쿼리 기능을 토픽 서비스에 구현.
    - 해당 기능에 대한 테스트 코드를 함께 작성함.

---
+++ src/main/java/com/HowBaChu/howbachu/service/TopicService.java
@@ -0,0 +1,14 @@
+package com.HowBaChu.howbachu.service;
+
+import com.HowBaChu.howbachu.domain.entity.Topic;
+
+import java.time.LocalDate;
+
+public interface TopicService {
+
+    /**
+     * 토픽 조회
+     */
+    public Topic getTopic(LocalDate date);
+
+}

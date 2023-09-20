47ae9fc77535340cd47429fe3e3971581783f3c1
dlrjs2360 <lhg3670@naver.com>
2023-09-20 15:18:07 +0900

    :recycle: refactor: quartz 제거 및 @Scheduled로 대체

--- src/main/java/com/HowBaChu/howbachu/HowBaChuApplication.java
+++ src/main/java/com/HowBaChu/howbachu/HowBaChuApplication.java
@@ -3,9 +3,11 @@ package com.HowBaChu.howbachu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class HowBaChuApplication {
 
--- src/main/java/com/HowBaChu/howbachu/service/impl/TopicServiceImpl.java
+++ src/main/java/com/HowBaChu/howbachu/service/impl/TopicServiceImpl.java
@@ -1,53 +1,31 @@
 package com.HowBaChu.howbachu.service.impl;
 
-import static org.quartz.CronScheduleBuilder.cronSchedule;
-import static org.quartz.JobBuilder.newJob;
-import static org.quartz.TriggerBuilder.newTrigger;
-
 import com.HowBaChu.howbachu.domain.dto.topic.TopicResponseDto;
 import com.HowBaChu.howbachu.domain.entity.embedded.VotingStatus;
 import com.HowBaChu.howbachu.repository.TopicRepository;
 import com.HowBaChu.howbachu.service.TopicService;
-import com.HowBaChu.howbachu.utils.quartz.VotingUpdateJob;
 import java.time.LocalDate;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.quartz.CronTrigger;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.impl.StdSchedulerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TopicServiceImpl implements TopicService {
 
     private final TopicRepository topicRepository;
 
     private VotingStatus votingStatus = new VotingStatus();
 
-    @PostConstruct
-    public void init() throws SchedulerException {
-        Scheduler schedule = new StdSchedulerFactory().getScheduler();
-
-        JobDetail votingUpdateJob = newJob(VotingUpdateJob.class)
-            .withIdentity("votingUpdate", "vote")
-            .build();
-
-        CronTrigger votingUpdateTrigger = newTrigger().withIdentity("votingTrigger","vote")
-            .withSchedule(cronSchedule("0 * * * * ?"))
-            .build();
-
-        schedule.scheduleJob(votingUpdateJob, votingUpdateTrigger);
-        schedule.start();
-    }
-
     @Override
+    @Scheduled(cron = "0 * * * * ?")
     public void votingUpdate() {
         votingStatus = topicRepository.getTopicByDate(LocalDate.now()).getVotingStatus();
+        log.info("votingStatus_Updated : {} / {}", votingStatus.getA(), votingStatus.getB());
     }
 
     @Override

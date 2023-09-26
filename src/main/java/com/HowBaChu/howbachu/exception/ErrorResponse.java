79c5e3da67b0ce484419b1c38c04e5bc106a72a5
dlrjs2360 <lhg3670@naver.com>
2023-09-26 23:53:30 +0900

    :recycle: refactor: 에러 response에서 불필요한 status 제거

--- src/main/java/com/HowBaChu/howbachu/exception/ErrorResponse.java
+++ src/main/java/com/HowBaChu/howbachu/exception/ErrorResponse.java
@@ -1,20 +1,17 @@
 package com.HowBaChu.howbachu.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 @Data
 @Builder
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
 
-    private int status;
-    private String message;
     private HttpStatus httpStatus;
+    private String message;
 
     public ErrorResponse(HttpStatus status, String message) {
         this.httpStatus = status;

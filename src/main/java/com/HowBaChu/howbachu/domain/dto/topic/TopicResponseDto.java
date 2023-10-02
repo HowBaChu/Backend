d9dd0109fb617be4f5077713054c8202385de44e
Thingk0 <audtjd9080@naver.com>
2023-10-02 17:41:37 +0900

    :bug: Topic 캐싱 적용 시 직렬화 문제 해결.
    - [x] Topic 의 LocalDate 부분이 Redis 로 데이터가 들어갈 때 직렬화 부분에서 문제가 발생하여, Jackson-Datatype 모듈을 통해서 직렬화 문제를 해결.

--- src/main/java/com/HowBaChu/howbachu/domain/dto/topic/TopicResponseDto.java
+++ src/main/java/com/HowBaChu/howbachu/domain/dto/topic/TopicResponseDto.java
@@ -5,20 +5,30 @@ import com.HowBaChu.howbachu.domain.entity.embedded.SubTitle;
 import com.HowBaChu.howbachu.domain.entity.embedded.VotingStatus;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.*;
+
+@Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TopicResponseDto {
 
     private Long id;
+
     private String title;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate date;
+
     private SubTitle subTitle;
+
     private VotingStatus votingStatus;
 
     public static TopicResponseDto of(Topic topic, VotingStatus votingStatus) {

aba40ac9a56916943db3c3d18555099318515c6d
dlrjs2360 <lhg3670@naver.com>
2023-10-06 18:10:00 +0900

    :bug: fix: cors 설정 임시 변경
    - 로컬의 cors 에러로 인해 연동 목적으로 전체 cors 설정으로 변경합니다.

--- src/main/java/com/HowBaChu/howbachu/config/CorsConfig.java
+++ src/main/java/com/HowBaChu/howbachu/config/CorsConfig.java
@@ -2,31 +2,18 @@ package com.HowBaChu.howbachu.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+            .allowedOriginPatterns("*")
             .allowedMethods("*")
-            .allowedOrigins("http://localhost:3000")
-            .allowedHeaders("Authorization")
+            .allowedHeaders("*")
             .allowCredentials(true);
     }
-
- /*   @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-        configuration.setAllowedMethods(
-            Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(List.of("Authorization"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }*/
-
 }

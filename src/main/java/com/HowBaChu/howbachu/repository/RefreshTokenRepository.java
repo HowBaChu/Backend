3a3950f49032fffef556bec2ef9171fc2eaa13c4
dlrjs2360 <lhg3670@naver.com>
2023-10-07 13:52:20 +0900

    :recycle: refactor: 리프레시 토큰의 구조를 변경합니다.
    - "이메일" : "토큰값" 의 구조를 반전시킴으로써 토큰값으로부터 이메일을 불러오도록 설정했습니다.

--- src/main/java/com/HowBaChu/howbachu/repository/RefreshTokenRepository.java
+++ src/main/java/com/HowBaChu/howbachu/repository/RefreshTokenRepository.java
@@ -1,7 +1,12 @@
 package com.HowBaChu.howbachu.repository;
 
 import com.HowBaChu.howbachu.domain.entity.RefreshToken;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByValue(String email);
+
+    void deleteByValue(String email);
 }

b2988189c53b1abca9fdb11892d1d279b6ef79f0
Thingk0 <audtjd9080@naver.com>
2023-11-19 19:04:52 +0900

    :recycle: OpinController 부분 리팩토링.

--- src/main/java/com/HowBaChu/howbachu/controller/OpinController.java
+++ src/main/java/com/HowBaChu/howbachu/controller/OpinController.java
@@ -6,15 +6,7 @@ import com.HowBaChu.howbachu.service.OpinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
@@ -26,39 +18,39 @@ public class OpinController {
 
     @PostMapping
     public ResponseEntity<?> postOpin(@RequestBody OpinRequestDto requestDto,
-        @ApiIgnore Authentication authentication) {
-        return ResponseCode.OPIN_SAVE.toResponse(
-            opinService.createOpin(requestDto, authentication.getName(), requestDto.getParentId()));
+                                      @ApiIgnore Authentication authentication) {
+        return ResponseCode.OPIN_SAVE.toResponse(opinService.createOpin(requestDto, authentication.getName(), requestDto.getParentId()));
     }
 
     @GetMapping
-    public ResponseEntity<?> listOpin(@RequestParam(name = "page", defaultValue = "0") int page) {
-        return ResponseCode.OPIN_LIST.toResponse(opinService.getOpinList(page));
+    public ResponseEntity<?> listOpin(@RequestParam(name = "page", defaultValue = "0") int page,
+                                      @ApiIgnore Authentication authentication) {
+        return ResponseCode.OPIN_LIST.toResponse(opinService.getOpinList(page, authentication.getName()));
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<?> listOpin(@PathVariable("id") String id) {
-        return ResponseCode.OPIN_LIST.toResponse(opinService.getOpinThread(Long.parseLong(id)));
+    public ResponseEntity<?> listOpin(@PathVariable("id") Long id,
+                                      @ApiIgnore Authentication authentication) {
+        return ResponseCode.OPIN_LIST.toResponse(opinService.getOpinThread(id, authentication.getName()));
     }
 
     @PatchMapping(value = "/{id}")
     public ResponseEntity<?> updateOpin(@PathVariable("id") String id,
-        @RequestBody OpinRequestDto requestDto, @ApiIgnore Authentication authentication) {
-        return ResponseCode.OPIN_UPDATE.toResponse(
-            opinService.updateOpin(requestDto, Long.parseLong(id), authentication.getName()));
+                                        @RequestBody OpinRequestDto requestDto,
+                                        @ApiIgnore Authentication authentication) {
+        return ResponseCode.OPIN_UPDATE.toResponse(opinService.updateOpin(requestDto, Long.parseLong(id), authentication.getName()));
     }
 
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<?> deleteOpin(@PathVariable("id") String id,
-        @ApiIgnore Authentication authentication) {
-        return ResponseCode.OPIN_DELETE.toResponse(
-            opinService.removeOpin(Long.parseLong(id), authentication.getName()));
+    @DeleteMapping
+    public ResponseEntity<?> deleteOpin(@RequestParam("opinId") Long opinId,
+                                        @ApiIgnore Authentication authentication) {
+        return ResponseCode.OPIN_DELETE.toResponse(opinService.removeOpin(opinId, authentication.getName()));
     }
 
-    @GetMapping(value = "/member")
-    public ResponseEntity<?> listMyOpin(@RequestParam(name = "id") Long memberId,
-        @RequestParam(name = "page", defaultValue = "0") int page) {
-        return ResponseCode.OPIN_LIST.toResponse(opinService.getOpinListForMember(memberId, page));
+    @GetMapping(value = "/my")
+    public ResponseEntity<?> listMyOpin(@RequestParam(name = "page", defaultValue = "0") int page,
+                                        @ApiIgnore Authentication authentication) {
+        return ResponseCode.OPIN_LIST.toResponse(opinService.getMyOpinList(page, authentication.getName()));
     }
 
 }

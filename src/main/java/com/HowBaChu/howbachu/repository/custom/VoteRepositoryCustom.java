955754890a673388554c76fd57db2e3f6b9a1164
Thingk0 <audtjd9080@naver.com>
2023-11-19 18:37:39 +0900

    :sparkles: 투표 여부 확인 및 투표 조회 SQL 수정.
    - [X] 기존 쿠키로 투표 여부를 반환했던 것을 별도로 투표 여부 조회하는 기능을 추가 -> 추후에 성능 최적화를 위해 캐싱 적용 예정.
    - [X] 리포지터리에서 데이터를 조회할 때 fetch* 로 통일.
    - [X] boolean 타입으로 조회할 때, Optional 타입 및 isPresent() 메서드를 활용

--- src/main/java/com/HowBaChu/howbachu/repository/custom/VoteRepositoryCustom.java
+++ src/main/java/com/HowBaChu/howbachu/repository/custom/VoteRepositoryCustom.java
@@ -2,10 +2,9 @@ package com.HowBaChu.howbachu.repository.custom;
 
 
 import com.HowBaChu.howbachu.domain.entity.Vote;
-import java.util.Optional;
 
 public interface VoteRepositoryCustom {
-    boolean findVoteByTopicAndMember(Long topicId, Long memberId);
+    boolean fetchVoteByTopicAndMember(Long topicId, Long memberId);
 
-    Optional<Vote> findVoteByEmail(String email);
+    Vote fetchVoteByEmail(String email);
 }
--- src/main/java/com/HowBaChu/howbachu/service/VoteService.java
+++ src/main/java/com/HowBaChu/howbachu/service/VoteService.java
@@ -2,8 +2,12 @@ package com.HowBaChu.howbachu.service;
 
 
 import com.HowBaChu.howbachu.domain.dto.vote.VoteRequestDto;
+import com.HowBaChu.howbachu.domain.dto.vote.VoteResponseDto;
+
 import javax.servlet.http.HttpServletResponse;
 
 public interface VoteService {
     Long voting(VoteRequestDto requestDto, String email, HttpServletResponse response);
+
+    VoteResponseDto getVotingStatus(String name);
 }

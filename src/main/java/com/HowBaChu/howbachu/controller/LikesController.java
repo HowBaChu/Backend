fd709575b1d27f70adaa71f241b2f59ba1521530
Thingk0 <audtjd9080@naver.com>
2023-11-19 18:45:55 +0900

    :necktie: 좋아요 기능 API 통일.
    - [X] 기존 코드의 문제는 좋아요 추가 및 삭제를 각각 POST, DELETE 메서드를 나누어서 API를 따로 호출해야하는 문제점 -> 하나의 메서드로 통일하여, 좋아요 여부를 확인하여 true 이면 삭제를, false 이면 추가하는 메서드를 호출하도록 수정.
    - [X] 좋아요 여부 메서드를 boolean 타입으로 변경 및 분리
    - [X]

--- src/main/java/com/HowBaChu/howbachu/controller/LikesController.java
+++ src/main/java/com/HowBaChu/howbachu/controller/LikesController.java
@@ -1,13 +1,19 @@
 package com.HowBaChu.howbachu.controller;
 
 import com.HowBaChu.howbachu.domain.constants.ResponseCode;
-import com.HowBaChu.howbachu.domain.dto.likes.LikesRequestDto;
+import com.HowBaChu.howbachu.exception.CustomException;
+import com.HowBaChu.howbachu.exception.constants.ErrorCode;
 import com.HowBaChu.howbachu.service.LikesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,17 +22,21 @@ public class LikesController {
 
     private final LikesService likesService;
 
-    @PostMapping
-    public ResponseEntity<?> addLikes(@RequestParam("opinId") Long opinId,
-                                      @ApiIgnore Authentication authentication) {
-        return ResponseCode.LIKES_ADD.toResponse(
-            likesService.addLikes(authentication.getName(), opinId));
+    private String loggedInEmail() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+            .map(Authentication::getName)
+            .orElseThrow(
+                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
+            );
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> cancelLikes(@RequestBody LikesRequestDto requestDto,
-                                         @ApiIgnore Authentication authentication) {
-        return ResponseCode.LIKES_CANCEL.toResponse(
-            likesService.cancelLikes(authentication.getName(), requestDto.getOpinId()));
+    @PostMapping
+    public ResponseEntity<?> toggleLikes(@RequestParam("opinId") Long opinId) {
+        /* 로그인한 회원의 이메일 */
+        String email = loggedInEmail();
+        return likesService.checkDuplicateLike(email, opinId)
+            ? ResponseCode.LIKES_CANCEL.toResponse(likesService.cancelLikes(email, opinId))
+            : ResponseCode.LIKES_ADD.toResponse(likesService.addLikes(email, opinId));
     }
+
 }

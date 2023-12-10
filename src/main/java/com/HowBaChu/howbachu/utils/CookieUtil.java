37b20523e94000b9439eb07d5be762f94089358c
dlrjs2360 <lhg3670@naver.com>
2023-12-10 23:10:11 +0900

    :bug: fix: Third Party Cookie fix
    쿠키의 Same Site 옵션을 수정하여 서로 다른 Origin의 서드파티 쿠키가 정상적으로 송수신되도록 설정했습니다.

--- src/main/java/com/HowBaChu/howbachu/utils/CookieUtil.java
+++ src/main/java/com/HowBaChu/howbachu/utils/CookieUtil.java
@@ -1,12 +1,10 @@
 package com.HowBaChu.howbachu.utils;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,19 +13,24 @@ import org.springframework.stereotype.Component;
 public class CookieUtil {
 
     public void setCookie(HttpServletResponse response, String key, String value) {
-        Cookie cookie = new Cookie(key, value);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        response.addCookie(cookie);
+        ResponseCookie responseCookie = ResponseCookie.from(key, value)
+            .httpOnly(true)
+            .path("/")
+            .secure(true)
+            .sameSite("None")
+            .build();
+        response.addHeader("Set-Cookie", responseCookie.toString());
     }
 
     public void setCookie(HttpServletResponse response, String key, String value, int expiredTime) {
-        Cookie cookie = new Cookie(key, value);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(expiredTime);  // 2주
-        response.addCookie(cookie);
-        log.info("만료 기한: " + expiredTime);
+        ResponseCookie responseCookie = ResponseCookie.from(key, value)
+            .httpOnly(true)
+            .path("/")
+            .secure(true)
+            .sameSite("None")
+            .maxAge(expiredTime)
+            .build();
+        response.addHeader("Set-Cookie", responseCookie.toString());
     }
 
     public void deleteCookie(HttpServletResponse response, String... keys) {
@@ -41,10 +44,4 @@ public class CookieUtil {
 
     }
 
-    public int getRemainTime() {
-        LocalDateTime now = LocalDateTime.now();
-        LocalTime endOfDay = LocalTime.of(23, 59, 59);
-        Duration duration = Duration.between(now.toLocalTime(), endOfDay);
-        return Integer.parseInt(duration.getSeconds() + "");
-    }
 }

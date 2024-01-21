bedd482383dfe42a69ff46f16fef8fdefd77d54b
Thingk0 <audtjd9080@naver.com>
2024-01-21 22:49:53 +0900

    :recycle: 토픽 검색 메소드 업데이트 및 세부 정보 표현 개선
    - [X] 'fetchTopicSearch' 메소드에서 반환되는 'TopicSearchResponseDto'에 'topicId', 'subTitle', 'votingStatus' 필드 추가
    - [X] 'subTitle' 및 'votingStatus' 필드에 대해 내장 객체로 표현 변경: 보다 상세한 정보 제공
    - [X] 토픽 검색 결과 페이징 처리 기능 개선

--- src/main/java/com/HowBaChu/howbachu/repository/impl/TopicRepositoryImpl.java
+++ src/main/java/com/HowBaChu/howbachu/repository/impl/TopicRepositoryImpl.java
@@ -1,29 +1,30 @@
 package com.HowBaChu.howbachu.repository.impl;
 
-import static com.HowBaChu.howbachu.domain.entity.QTopic.topic;
-
 import com.HowBaChu.howbachu.domain.dto.search.SearchResultResponseDto;
 import com.HowBaChu.howbachu.domain.dto.topic.TopicResponseDto;
 import com.HowBaChu.howbachu.domain.entity.Topic;
+import com.HowBaChu.howbachu.domain.entity.embedded.SubTitle;
+import com.HowBaChu.howbachu.domain.entity.embedded.VotingStatus;
 import com.HowBaChu.howbachu.exception.CustomException;
 import com.HowBaChu.howbachu.exception.constants.ErrorCode;
 import com.HowBaChu.howbachu.repository.Support.Querydsl4RepositorySupport;
 import com.HowBaChu.howbachu.repository.custom.TopicRepositoryCustom;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.QBean;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.lang.Nullable;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.HowBaChu.howbachu.domain.entity.QTopic.topic;
 
 
 public class TopicRepositoryImpl extends Querydsl4RepositorySupport implements TopicRepositoryCustom {
@@ -37,7 +38,7 @@ public class TopicRepositoryImpl extends Querydsl4RepositorySupport implements T
     public Topic getTopicByDate(@Nullable LocalDate date) {
         return Optional.ofNullable(
             selectFrom(topic)
-                .where(searchByDate(date))
+                .where(topic.date.eq(Optional.ofNullable(date).orElse(LocalDate.now())))
                 .fetchOne()
         ).orElseThrow(() -> new CustomException(ErrorCode.TOPIC_NOT_FOUND));
     }
@@ -70,14 +71,14 @@ public class TopicRepositoryImpl extends Querydsl4RepositorySupport implements T
     public Page<SearchResultResponseDto.TopicSearchResponseDto> fetchTopicSearch(String condition, Pageable pageable) {
         List<SearchResultResponseDto.TopicSearchResponseDto> content =
             select(mapToTopicSearchResponseDto())
-            .from(topic)
-            .where(topic.title.contains(condition)
-                .or(topic.subTitle.sub_A.contains(condition))
-                .or(topic.subTitle.sub_B.contains(condition)))
-            .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
-            .orderBy(topic.date.desc())
-            .fetch();
+                .from(topic)
+                .where(topic.title.contains(condition)
+                    .or(topic.subTitle.sub_A.contains(condition))
+                    .or(topic.subTitle.sub_B.contains(condition)))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(topic.date.desc())
+                .fetch();
 
         JPAQuery<Long> countQuery = select(topic.count())
             .from(topic)
@@ -88,18 +89,21 @@ public class TopicRepositoryImpl extends Querydsl4RepositorySupport implements T
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
     }
 
-    private BooleanExpression searchByDate(LocalDate date) {
-        return topic.date.eq(Optional.ofNullable(date).orElse(LocalDate.now()));
-    }
 
     private static QBean<SearchResultResponseDto.TopicSearchResponseDto> mapToTopicSearchResponseDto() {
         return Projections.fields(SearchResultResponseDto.TopicSearchResponseDto.class,
+            topic.id.as("topicId"),
             topic.title.as("title"),
-            topic.subTitle.sub_A.as("subTitleA"),
-            topic.subTitle.sub_B.as("subTitleB"),
-            topic.votingStatus.A.as("countA"),
-            topic.votingStatus.B.as("countB"),
+            Projections.fields(SubTitle.class,
+                topic.subTitle.sub_A.as("sub_A"),
+                topic.subTitle.sub_B.as("sub_B")
+            ).as("subTitle"),
+            Projections.fields(VotingStatus.class,
+                topic.votingStatus.A.as("A"),
+                topic.votingStatus.B.as("B")
+            ).as("votingStatus"),
             topic.date.as("date")
         );
     }
+
 }

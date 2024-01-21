f09d9d345a916db4fea7a1cb5a8156e09980523e
Thingk0 <audtjd9080@naver.com>
2024-01-21 22:37:19 +0900

    :recycle: 인증 및 페이징 기능 추가
    - [X] 모든 검색 관련 메소드에 'Authentication' 객체 추가: 사용자 인증 정보 활용
    - [X] 'search', 'searchMoreParentOpin', 'searchMoreChildOpin' 메소드에 기본 페이징 처리 적용: 검색 결과의 페이지네이션 지원
    - [X] 'searchMoreTopic' 메소드에 페이징 처리 추가: 주제별 검색 결과 페이징

--- src/main/java/com/HowBaChu/howbachu/controller/SearchController.java
+++ src/main/java/com/HowBaChu/howbachu/controller/SearchController.java
@@ -4,8 +4,10 @@ import com.HowBaChu.howbachu.domain.constants.ResponseCode;
 import com.HowBaChu.howbachu.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,27 +21,31 @@ public class SearchController {
     private final SearchService searchService;
 
     @GetMapping
-    public ResponseEntity<?> search(@Nullable @RequestParam("cond") String cond) {
-        return ResponseCode.SEARCH_SUCCESS.toResponse(searchService.fetchSearchResult(cond));
+    public ResponseEntity<?> search(Authentication authentication,
+                                    @Nullable @RequestParam("cond") String cond,
+                                    @PageableDefault(size = 5) Pageable pageable) {
+        return ResponseCode.SEARCH_SUCCESS.toResponse(searchService.fetchSearchResult(cond, authentication.getName(), pageable));
     }
 
     @GetMapping(value = "/parent-opin/")
-    public ResponseEntity<?> searchMoreParentOpin(@Nullable @RequestParam("cond") String cond,
-        Pageable pageable) {
+    public ResponseEntity<?> searchMoreParentOpin(Authentication authentication,
+                                                  @Nullable @RequestParam("cond") String cond,
+                                                  @PageableDefault(size = 5) Pageable pageable) {
         return ResponseCode.SEARCH_SUCCESS.toResponse(
-            searchService.fetchMoreParentOpins(cond, pageable));
+            searchService.fetchMoreParentOpins(cond, authentication.getName(), pageable));
     }
 
     @GetMapping(value = "/child-opin/")
-    public ResponseEntity<?> searchMoreChildOpin(@Nullable @RequestParam("cond") String cond,
-        Pageable pageable) {
+    public ResponseEntity<?> searchMoreChildOpin(Authentication authentication,
+                                                 @Nullable @RequestParam("cond") String cond,
+                                                 @PageableDefault(size = 5) Pageable pageable) {
         return ResponseCode.SEARCH_SUCCESS.toResponse(
-            searchService.fetchMoreChildOpins(cond, pageable));
+            searchService.fetchMoreChildOpins(cond, authentication.getName(), pageable));
     }
 
     @GetMapping(value = "/topic/")
     public ResponseEntity<?> searchMoreTopic(@Nullable @RequestParam("cond") String cond,
-        Pageable pageable) {
+                                             @PageableDefault(size = 5) Pageable pageable) {
         return ResponseCode.SEARCH_SUCCESS.toResponse(
             searchService.fetchMoreTopics(cond, pageable));
     }
